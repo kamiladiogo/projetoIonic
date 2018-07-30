f2385809751d19cc495d1ce48b6bbab7d2cc3c22
Kamila Diogo <kamiladiogo@hotmail.com>
2018-07-30 16:01:33 -0300

    nova pagina imc

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
@@ -8,30 +8,23 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  peso: number;
+  altura: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(public alertaCtrl: AlertController) {
+  
   }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
-  }
+calcular()
+{
+    
+  const alerta = this.alertaCtrl.create
+ (
+    
+    {
+    title: 'IMC: ',
+    subTitle: 'O seu IMC é: ' + ((this.peso * this.altura)/2),
+    buttons: ['ok']
+  });
+  alerta.present();
+}
 }
